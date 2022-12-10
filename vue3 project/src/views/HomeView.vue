aae17a3527003d98cf211c5ebfd0b3aa27cfe218
francis556 <vikrammehra244@gmail.com>
2022-12-10 05:38:02 +0200

    script setup

--- vue3 project/src/views/HomeView.vue
+++ vue3 project/src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup>
-import TheWelcome from "../components/TheWelcome.vue";
+import TheWelcome from "@/components/TheWelcome.vue";
+
+    
+let message = $ref("Hello world");
+
+setTimeout(() => {
+  message = "I have been changed";
+},2000);
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <p>{{ message }}</p>
+    <input type="text" v-model="message" />
   </main>
 </template>
