b5e25e0b727d2770de76ddfa0cecbef2ae608564
francis556 <vikrammehra244@gmail.com>
2022-12-11 16:52:28 +0200

    use composable example

--- vue3 project/src/views/HomeView.vue
+++ vue3 project/src/views/HomeView.vue
@@ -1,14 +1,27 @@
 <script setup>
-import {useFlash} from "@/composables/useFlash"
 
-let {flash} = useFlash();
+  import { ref, watch } from 'vue';
+
+  import { useStorage } from "@/composables/useStorage";
+
+  let food = useStorage('food');
+
+  let age = useStorage('age', 10);
+
 
 </script>
 
 <template>
+
   <main>
+
     <p>
-      <button @click="flash('Test','It works')">Click me</button>
+      What is your favorite food? <input type="text" v-model="food" />
     </p>
+
+    <p>
+      How old are you? <input type="text" v-model="age" />
+    </p>
+
   </main>
 </template>
