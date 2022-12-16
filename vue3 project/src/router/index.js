18a0a97597f8337b711b79a0ca43f71848d1b4e8
francis556 <vikrammehra244@gmail.com>
2022-12-16 13:01:41 +0200

    counter view and route

--- vue3 project/src/router/index.js
+++ vue3 project/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ContactView from "../views/ContactView.vue"
+import CounterView from "../views/CounterView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/counter",
+      name: "counter",
+      component: CounterView,
     },
     {
       path: "/about",
---
+++ vue3 project/src/views/CounterView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <div>
+        <h1>{{ count }}</h1>
+    </div>
+
+</template>
